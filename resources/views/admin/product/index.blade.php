452f1fbec59d402c5d8fdce18175f726a65eb723
Kazuki Tobita <kazukiti201@gmail.com>
2018-11-13 16:54:13 +0900

    Modify views admin/product

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -2,39 +2,80 @@
 
 @section('content')
     <div class="container">
-        商品一覧
-
-        <form method="get" action="" >
-            <input type="text" size="31"  >
-            <input type="submit"  value="検索">
+        <h1 class="h1">商品一覧</h1>
+        <form method="post" action="">
+            <label for=""></label>
+            <input type="text" placeholder="商品ID" class="form-control col-md-8 d-inline mb-4" autofocus>
+            <button type="submit" class="btn btn-primary">検索</button>
         </form>
 
-        <table>
+        <table class="table table-striped mb-4">
             <tr>
-                <td>XXXX</td>
-                <td>YYYY/MM/DD</td>
-                <td><input type="button" value="詳細"></td>
+                <th>商品ID</th>
+                <th>商品名</th>
+                <th>価格</th>
+                <th>登録者</th>
+                <th>登録日</th>
+                <th>&nbsp;</th>
+                <th>&nbsp;</th>
+                <th>&nbsp;</th>
             </tr>
+
             <tr>
-                <td>XXXX</td>
+                <td>1</td>
+                <td>タウリン</td>
+                <td>100</td>
+                <td>ミジュマル</td>
                 <td>YYYY/MM/DD</td>
-                <td><input type="button" value="詳細"></td>
+                <td>
+                    <a href="" class="btn btn-success">編集</a>
+                </td>
+                <td>
+                    <a href="" class="btn btn-danger">削除</a>
+                </td>
+                <td>
+                    <a href="" class="btn btn-primary">詳細</a>
+                </td>
             </tr>
+
             <tr>
-                <td>XXXX</td>
+                <td>1</td>
+                <td>タウリン</td>
+                <td>100</td>
+                <td>ミジュマル</td>
                 <td>YYYY/MM/DD</td>
-                <td><input type="button" value="詳細"></td>
+                <td>
+                    <a href="" class="btn btn-success">編集</a>
+                </td>
+                <td>
+                    <a href="" class="btn btn-danger">削除</a>
+                </td>
+                <td>
+                    <a href="" class="btn btn-primary">詳細</a>
+                </td>
             </tr>
+
             <tr>
-                <td>XXXX</td>
+                <td>1</td>
+                <td>タウリン</td>
+                <td>100</td>
+                <td>ミジュマル</td>
                 <td>YYYY/MM/DD</td>
-                <td><input type="button" value="詳細"></td>
+                <td>
+                    <a href="" class="btn btn-success">編集</a>
+                </td>
+                <td>
+                    <a href="" class="btn btn-danger">削除</a>
+                </td>
+                <td>
+                    <a href="" class="btn btn-primary">詳細</a>
+                </td>
             </tr>
-            <tr>
-                <td>XXXX</td>
-                <td>YYYY/MM/DD</td>
-                <td><input type="button" value="詳細"></td>
-            </tr>
+
         </table>
+
+        <div class="text-right">
+            <a href="" class="btn btn-primary">商品を追加する</a>
+        </div>
     </div>
 @endsection
