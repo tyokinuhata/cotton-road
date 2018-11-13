5cfe558f9e3ff949fc81d3e6c19f41246db5d0b1
ryota <uta09007@yahoo.co.jp>
2018-11-13 17:04:23 +0900

    ユーザー情報変更画面の編集

--- resources/views/customer/user/change.blade.php
+++ resources/views/customer/user/change.blade.php
@@ -1,19 +1,46 @@
-ユーザ情報変更
-<div >
-    <form>
-        ユーザID:
-        <input type="text" size="30"/>
-    </form>
-    <form>
-        ID:
-        <input type="text" size="30"/>
-    </form>
-    <form>
-        登録日:
-        <input type="text" size="30"/>
+@extends('layouts.app')
 
-    </form>
+@section('content')
 
-    <input type="button" value="変更" >
-</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('ユーザー情報変更') }}</div>
+                    <div class="card-body">
 
+                        <div class="form-group row">
+                            <label for="user_id" class="col-md-2 col-form-label text-md-right">{{ __('ユーザーID') }}</label>
+                            <div class="col-md-6">
+                                <input id="user_id" type="text" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="user_id" class="col-md-2 col-form-label text-md-right">{{ __('ID') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="user_id" type="text" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="user_id" class="col-md-2 col-form-label text-md-right">{{ __('登録日') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="user_id" type="text" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-3 text-right">
+                                <button class="btn btn-primary">
+                                    {{ __('変更') }}</button></div>
+                        </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
