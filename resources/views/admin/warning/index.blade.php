e9c6651d836c898e1fbe138d20d94c97bc779684
ryota <uta09007@yahoo.co.jp>
2018-11-13 15:48:44 +0900

    画面の作成

--- resources/views/admin/warning/index.blade.php
+++ resources/views/admin/warning/index.blade.php
@@ -1,1 +1,15 @@
-強制退会
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        強制退会
+        <div>
+
+            <form>
+                ID:
+                <input type="text" size="30"/>
+            </form>
+
+            <input type="button" value="退会"/></div>
+    </div>
+@endsection
