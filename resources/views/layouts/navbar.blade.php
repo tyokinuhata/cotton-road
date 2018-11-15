45dbb4ce2e26b5df10ffe7dccecfffde05dc4eda
Kazuki Tobita <kazukiti201@gmail.com>
2018-11-16 01:21:39 +0900

    Add sidebar

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+    <div class="container">
+        <span>Cotton Road</span>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Signin') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        @if (Route::has('register'))
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Signup') }}</a>
+                        @endif
+                    </li>
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" role="button">{{ Auth::user()->username }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('signout-form').submit();">
+                            {{ __('Signout') }}
+                        </a>
+
+                        <form id="signout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav>
