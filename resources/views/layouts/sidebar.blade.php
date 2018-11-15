6156391b1c27bf1d0df2a1613e6c2e641680e65c
Kazuki Tobita <kazukiti201@gmail.com>
2018-11-16 02:26:25 +0900

    Modify sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
-<div class="container-fluid h-100">
+<div class="container-fluid" style="height: 90%">
     <div class="row h-100">
-        <nav class="col-md-2 d-none d-md-block sidebar border-right h-100">
+        <nav class="col-md-2 d-none d-md-block sidebar border-right">
             <div class="sidebar-sticky">
                 <ul class="nav flex-column">
                     <li class="nav-item">
@@ -19,7 +19,7 @@
             </div>
         </nav>
 
-        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+        <main role="main" class="col-md-9 col-lg-10 pt-3 px-4">
             @yield('content')
         </main>
     </div>
