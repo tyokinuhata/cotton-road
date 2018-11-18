77acd6b155159dfd70b6e32b28b720699bf41227
Kazuki Tobita <kazukiti201@gmail.com>
2018-11-18 09:27:42 +0900

    Modify view (admin/orders)

--- resources/views/admin/orders/wait.blade.php
+++ resources/views/admin/orders/wait.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <h1 class="h1">入庫待ち一覧</h1>
+        <h1 class="h1 mb-3">入庫待ち一覧</h1>
         <table class="table table-striped">
             <tr>
                 <th>商品ID</th>
@@ -10,7 +10,7 @@
                 <th>発注先企業名</th>
                 <th>単価</th>
                 <th>個数</th>
-                <th>金額</th>
+                <th>合計金額</th>
                 <th>発注日</th>
                 <th>&nbsp;</th>
             </tr>
@@ -19,9 +19,9 @@
                 <td>1</td>
                 <td>タウリン</td>
                 <td>ポケモンセンター</td>
-                <td>100</td>
+                <td>¥100</td>
                 <td>10</td>
-                <td>1000</td>
+                <td>¥1000</td>
                 <td>YYYY/MM/DD</td>
                 <td>
                     <a href="" class="btn btn-primary">詳細</a>
@@ -29,7 +29,7 @@
             </tr>
 
             <tr>
-                <td>1</td>
+                <td>2</td>
                 <td>タウリン</td>
                 <td>ポケモンセンター</td>
                 <td>100</td>
@@ -42,7 +42,7 @@
             </tr>
 
             <tr>
-                <td>1</td>
+                <td>3</td>
                 <td>タウリン</td>
                 <td>ポケモンセンター</td>
                 <td>100</td>
