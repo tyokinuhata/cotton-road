4b643a012e76948f820c7b887f312341cc5a5b50
Kazuki Tobita <kazukiti201@gmail.com>
2018-11-20 15:40:01 +0900

    Modify view (seller/delivery)

--- resources/views/seller/delivery/yet.blade.php
+++ resources/views/seller/delivery/yet.blade.php
@@ -2,50 +2,61 @@
 
 @section('content')
     <div class="container">
-        <h1 class="h1">未納品一覧</h1>
+        <h1 class="h1 mb-3">未納品一覧</h1>
         <table class="table table-striped">
             <tr>
                 <th>商品ID</th>
                 <th>商品名</th>
-                <th>納品先顧客</th>
-                <th>注文日</th>
+                <th>納品先顧客名</th>
+                <th>価格</th>
+                <th>納品数</th>
+                <th>合計金額</th>
                 <th>&nbsp;</th>
                 <th>&nbsp;</th>
             </tr>
-
             <tr>
                 <td>1</td>
                 <td>タウリン</td>
-                <td>ポケモンセンター</td>
-                <td>YYYY/MM/DD</td>
+                <td>ミジュマル</td>
+                <td>¥100</td>
+                <td>10</td>
+                <td>¥1000</td>
                 <td>
-                    <a href="" class="btn btn-danger">納品</a>
+                    <form method="POST" action="">
+                        <button type="submit" class="btn btn-danger">納品</button>
+                    </form>
                 </td>
                 <td>
                     <a href="" class="btn btn-primary">詳細</a>
                 </td>
             </tr>
-
             <tr>
-                <td>1</td>
+                <td>2</td>
                 <td>タウリン</td>
-                <td>ポケモンセンター</td>
-                <td>YYYY/MM/DD</td>
+                <td>ミジュマル</td>
+                <td>¥100</td>
+                <td>10</td>
+                <td>¥1000</td>
                 <td>
-                    <a href="" class="btn btn-danger">納品</a>
+                    <form method="POST" action="">
+                        <button type="submit" class="btn btn-danger">納品</button>
+                    </form>
                 </td>
                 <td>
                     <a href="" class="btn btn-primary">詳細</a>
                 </td>
             </tr>
-
             <tr>
-                <td>1</td>
+                <td>3</td>
                 <td>タウリン</td>
-                <td>ポケモンセンター</td>
-                <td>YYYY/MM/DD</td>
+                <td>ミジュマル</td>
+                <td>¥100</td>
+                <td>10</td>
+                <td>¥1000</td>
                 <td>
-                    <a href="" class="btn btn-danger">納品</a>
+                    <form method="POST" action="">
+                        <button type="submit" class="btn btn-danger">納品</button>
+                    </form>
                 </td>
                 <td>
                     <a href="" class="btn btn-primary">詳細</a>
@@ -53,5 +64,4 @@
             </tr>
         </table>
     </div>
-
 @endsection
