6384bfb2f81b746598fa88a5ed4c244c5331cf35
unknown <B7203@oic.osaka.jp>
2018-11-20 14:20:46 +0900

    customerユーザー情報編集の更新

--- resources/views/customer/user/change.blade.php
+++ resources/views/customer/user/change.blade.php
@@ -1,46 +1,59 @@
+
 @extends('layouts.base')
 
 @section('content')
-
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('ユーザー情報変更') }}</div>
-                    <div class="card-body">
-
-                        <div class="form-group row">
-                            <label for="user_id" class="col-md-2 col-form-label text-md-right">{{ __('ユーザーID') }}</label>
-                            <div class="col-md-6">
-                                <input id="user_id" type="text" class="form-control">
-                            </div>
+        <h1 class="h1 mb-3">ユーザ情報編集</h1>
+        <div class="mb-2">
+            <div class="mb-2">
+                <img src="http://placehold.it/200x200&text=icon" alt="">
+            </div>
+            <form method="POST" action="">
+                @csrf
+                <div class="row mb-2">
+                    <label for="user_id" class="col-md-2">ユーザID</label>
+                    <input type="text" id="user_id" name="user_id" class="form-control d-inline col-md-7" required>
+                </div>
+                <div class="row mb-2">
+                    <label for="username" class="col-md-2">ユーザ名</label>
+                    <input type="text" id="username" name="username" class="form-control d-inline col-md-7" required>
+                </div>
+                <div class="row mb-2">
+                    <label class="col-md-2">性別</label>
+                    <div class="col-md-7">
+                        <div class="d-inline">
+                            <input type="radio" name="sex" id="man" value="man" checked required>
+                            <label class="form-check-label" for="man">男</label>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="user_id" class="col-md-2 col-form-label text-md-right">{{ __('ID') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="user_id" type="text" class="form-control">
-                            </div>
+                        <div class="d-inline">
+                            <input type="radio" name="sex" id="woman" value="woman" required>
+                            <label class="form-check-label" for="woman">女</label>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="user_id" class="col-md-2 col-form-label text-md-right">{{ __('登録日') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="user_id" type="text" class="form-control">
-                            </div>
+                        <div class="d-inline">
+                            <input type="radio" name="sex" id="other" value="other" required>
+                            <label class="form-check-label" for="other">その他</label>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-3 text-right">
-                                <button class="btn btn-primary">
-                                    {{ __('変更') }}</button></div>
-                        </div>
-                        </form>
                     </div>
                 </div>
+                <div class="row mb-2">
+                    <label for="address" class="col-md-2">住所</label>
+                    <input type="text" id="address" name="address" class="form-control d-inline col-md-7" required>
+                </div>
+                <div class="row mb-2">
+                    <label for="age" class="col-md-2">年齢</label>
+                    <input type="number" id="age" name="age" class="form-control d-inline col-md-7" required>
+                </div>
+                <div class="row mb-2">
+                    <label for="email" class="col-md-2">メールアドレス</label>
+                    <input type="email" id="email" name="email" class="form-control d-inline col-md-7" required>
+                </div>
+            </form>
+            <div>
+                <button class="btn btn-success">保存</button>
             </div>
+        </div>
+        <div>
+            <a href="{{ url('/customer/user') }}">< 戻る</a>
         </div>
     </div>
 @endsection
