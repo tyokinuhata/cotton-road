184564664d1989a63aad15f378c980b056208ac5
Kazuki Tobita <kazukiti201@gmail.com>
2019-01-13 12:15:34 +0900

    Refine code

--- resources/views/admin/orders/newly.blade.php
+++ resources/views/admin/orders/newly.blade.php
@@ -6,7 +6,7 @@
 
         <ul class="nav nav-tabs mb-4">
             <li class="nav-item">
-                <a href="#unapproved" class="nav-link active" id="unapproved-tab">未承認</a>
+                <a href="#unapproved" class="nav-link" id="unapproved-tab">未承認</a>
             </li>
             <li class="nav-item">
                 <a href="#waitContainer" class="nav-link" id="waitContainer-tab">コンテナ待ち</a>
@@ -20,7 +20,7 @@
         </ul>
 
         <div class="tab-content">
-            <div id="unapproved" class="tab-pane active">
+            <div id="unapproved" class="tab-pane">
                 <div>
                     @include('admin.orders.newly.unapproved')
                 </div>
@@ -49,10 +49,13 @@
     {{--ページネーションとタブを共存させる魔法のコードです--}}
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script>
-      let statuses = [ 'unapproved', 'waitContainer', 'waitBack', 'waitDisposal' ]
-      let currentStatus = 'unapproved'
-
       $(function() {
+        let statuses = [ 'unapproved', 'waitContainer', 'waitBack', 'waitDisposal' ]
+        let currentStatus = sessionStorage.getItem('newlyTab') ? sessionStorage.getItem('newlyTab') : 'unapproved'
+
+        $('#' + currentStatus + '-tab').addClass('active')
+        $('#' + currentStatus).addClass('active')
+
         for (let status of statuses) {
           $('#' + status + '-tab').on('click', function () {
             $('#' + currentStatus + '-tab').removeClass('active')
@@ -61,9 +64,9 @@
             $('#' + currentStatus).removeClass('active')
             $('#' + status).addClass('active')
             currentStatus = status
+            sessionStorage.setItem('newlyTab', status)
           })
         }
       })
     </script>
 @endsection
-
