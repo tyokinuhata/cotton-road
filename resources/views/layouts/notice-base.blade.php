5b705e4e48074b36a7cbcc7206b1dbb3eb044798
Kazuki Tobita <kazukiti201@gmail.com>
2019-01-18 02:58:39 +0900

    Fix notice bug

--- resources/views/layouts/notice-base.blade.php
+++ resources/views/layouts/notice-base.blade.php
@@ -4,6 +4,11 @@
     <div class="container">
         <h1 class="h1 mb-3">通知</h1>
         @include($notice->message)
-        <a href="{{ url('/seller/info/notice') }}">< 戻る</a>
+        @if (Auth::user()->type === 'admin' || Auth::user()->type === 'employee')
+            <a href="{{ url('/admin/info/notice') }}">< 戻る</a>
+        @endif
+        @seller (Auth::user()->type)
+            <a href="{{ url('/seller/info/notice') }}">< 戻る</a>
+        @endseller
     </div>
 @endsection
