e716d4502158a8634bc3f55d59fc04ae8b20da4c
Kazuki Tobita <kazukiti201@gmail.com>
2019-01-19 08:59:42 +0900

    Modify view

--- resources/views/seller/delivery/cardboard/apply.blade.php
+++ resources/views/seller/delivery/cardboard/apply.blade.php
@@ -11,7 +11,9 @@
                         <option value="{{ $cardboard->id }}">{{ $cardboard->size }}</option>
                     @endforeach
                 </select>
+                <span class="ml-1 mb-0 mt-2">サイズ ×</span>
                 <input type="number" value="1" name="number" class="form-control col-md-1 ml-1" min="1" max="10" required>
+                <span class="ml-1 mb-0 mt-2">個</span>
                 <input type="hidden" name="user_id" value="{{ Auth::id() }}">
                 <button type="submit" class="btn btn-primary ml-1">申請</button>
             </div>
